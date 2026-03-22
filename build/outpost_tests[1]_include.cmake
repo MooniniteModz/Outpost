708110692fba6fd6de8e7a22d924aa5fbb05295e
mooninitemodz@proton.me <mooninitemodz@proton.me>
2026-03-22 01:19:08 -0500

    ZZ

--- build/outpost_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/moon/UPT-Outpost/build/outpost_tests[1]_tests.cmake")
-  include("/home/moon/UPT-Outpost/build/outpost_tests[1]_tests.cmake")
-else()
-  add_test(outpost_tests_NOT_BUILT outpost_tests_NOT_BUILT)
-endif()
